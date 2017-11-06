749d59398c311abeefe4198143c5e98a0fe8016a
Riccardo Casatta <riccardo@casatta.it>
2017-11-06 11:42:40 +0100

    welcome bonus already sent check on server side

--- welcome_bonus.py
+++ welcome_bonus.py
@@ -1,34 +1,90 @@
 import bitcoin.rpc
 from twisted.internet import reactor
 from twisted.web import resource, server
+import requests
+import os
+
+API_KEY = os.environ['WELCOME_BONUS_API_KEY']
+VALUE = os.environ['WELCOME_BONUS_VALUE']
+ENDPOINT = os.environ['WELCOME_BONUS_API_ENDPOINT']
 
 class MyResource(resource.Resource):
     isLeaf = True
 
-    def render_GET(self, request):
+    @staticmethod
+    def get_address(phone):
+        r = requests.get(ENDPOINT + '/pn2a/v1/address/' + phone)
+        if r.status_code != 200:
+            raise LookupError
+        address = r.json()['value']['address']
+        return address
+
+    @staticmethod
+    def is_welcome_bonus_sent(phone):
+        headers = {'api_key': API_KEY}
+        r = requests.get(ENDPOINT + '/pn2a/v1/welcome_bonus/' + phone, headers=headers)
+        if r.status_code != 200:
+            raise LookupError
+        print(r.json())
+        code = r.json()['code']
+        if code == 118021:
+            return False
+        else:
+            if code == 118020:
+                return True
+        raise Exception
+
+    @staticmethod
+    def set_welcome_bonus(phone):
+        headers = {'api_key': API_KEY}
+        r = requests.post(ENDPOINT + '/pn2a/v1/welcome_bonus/' + phone, headers=headers)
+        if r.status_code != 200:
+            raise LookupError
+        print(r.json())
+        code = r.json()['code']
+        if code == 118022:
+            return True
+        raise Exception
+
+    @staticmethod
+    def send_welcome_bonus(address):
         proxy = bitcoin.rpc.Proxy()
         print(proxy.getinfo())
-        print(request)
-        address = request.postpath[0].decode('utf-8')
-        print(address)
         isvalid = proxy._call('validateaddress', str(address))['isvalid']
-
-        print(isvalid)
+        print("Is address valid?" + isvalid)
         if isvalid:
-            try:
-                decode = request.postpath[1].decode('utf-8')
-                print(decode)
-                value = int(decode)
-            except:
-                value = 100000
-
-            proxy.sendtoaddress(address, value)
+            proxy.sendtoaddress(address, VALUE)
             return 'sent'
         else:
             return 'invalid address'
 
+    def render_GET(self, request):
+        phone = request.postpath[0].decode('utf-8')
+        try:
+            address = MyResource.get_address(phone)
+        except:
+            err = "can't lookup address from phone"
+            print(err)
+            return err
+
+        print("Lookup address " + address)
+
+        try:
+            sent = MyResource.is_welcome_bonus_sent(phone)
+        except:
+            err = "can't check if welcome bonus was already sent"
+            print(err)
+            return err
+
+        if sent:
+            to_phone = "welcome bonus already sent to " + phone
+            print(to_phone)
+            return to_phone
+
+        return MyResource.send_welcome_bonus(address)
+
 
 site = server.Site(MyResource())
 
-reactor.listenTCP(8000, site)
+reactor.listenTCP(80, site)
 reactor.run()
